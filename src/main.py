4ae8f0cc4e098b47698cf469a0a901f286df0719
Sh1ra0ri <xxnon.7@gmail.com>
2025-05-22 12:57:46 +0300

    14.2

--- src/main.py
+++ src/main.py
@@ -1,14 +1,44 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, value: float):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
+
+    @classmethod
+    def new_product(
+        cls, product_data: dict, existing_products: list = None
+    ) -> "Product":
+        name = product_data.get("name")
+        description = product_data.get("description", "")
+        price = product_data.get("price", 0.0)
+        quantity = product_data.get("quantity", 0)
+
+        if existing_products:
+            for product in existing_products:
+                if product.name == name:
+                    product.quantity += quantity
+                    if price > product.price:
+                        product.price = price
+                    return product
+
+        return cls(name, description, price, quantity)
 
 
 class Category:
@@ -17,15 +47,26 @@ class Category:
 
     name: str
     description: str
-    products: list
 
     def __init__(self, name: str, description: str, products: list = None):
         self.name = name
         self.description = description
-        self.products = products if products is not None else []
+        self.__products = products if products is not None else []
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
 
     def add_product(self, product: Product):
-        self.products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise ValueError
+
+    @property
+    def products(self) -> str:
+        if not self.__products:
+            return "Категория пуста"
+        return "\n".join(
+            f"{product.name}, {product.price} руб. " f"Остаток: {product.quantity} шт."
+            for product in self.__products
+        )
